209e6d3de663f7db2bb16a60e399e5dcaaf734aa
MaxiGomez5431 <mamitgomez@gmail.com>
2026-01-19 21:43:48 -0300

    Draw sprite from memory implemented

--- chip8.js
+++ chip8.js
@@ -2,7 +2,7 @@ import { createLoop } from './loop.js';
 
 const displayElement = document.getElementById('chip8-display');
 
-class Chip8Display {
+class Chip8 {
   constructor(canvas) {
     this.memory = new Uint8Array(4096);
     this.V = new Uint8Array(16);
@@ -16,6 +16,7 @@ class Chip8Display {
     this.width = 64;
     this.height = 32;
     this.keys = []
+    this.debugDisplay = new Uint8Array(64 * 32).fill(0);
     
     this.timersLoop
     this.emulationLoop
@@ -51,9 +52,6 @@ class Chip8Display {
     canvas.height = 32 * this.SCALE;
 
     this.canvasContext = canvas.getContext('2d');
-
-    this.canvasContext.fillStyle = 'white'; //revisar, creo que el coloreado de pixeles tiene que ser xor, no solo blanco
-    this.canvasContext.fillRect(31 * this.SCALE, 14 * this.SCALE, this.SCALE, this.SCALE);
   }
   
   getFirstNibble(opcode) {
@@ -88,17 +86,17 @@ class Chip8Display {
       0x0: () => this.family0(opcode, NNN),
       0x1: () => this.jumpTo(NNN),
       0x2: () => this.subrutineAt(NNN),
-      0x3: undefined, //Skips the next instruction if VX equals NN (usually the next instruction is a jump to skip a code block).
-      0x4: undefined, //Skips the next instruction if VX does not equal NN (usually the next instruction is a jump to skip a code block).
-      0x5: undefined, //Skips the next instruction if VX equals VY (usually the next instruction is a jump to skip a code block).
-      0x6: undefined, //Sets VX to NN.
-      0x7: undefined, //Adds NN to VX (carry flag is not changed).
+      0x3: () => this.skipNextInstructionIf(this.V[X] === NN), //Skips the next instruction if VX equals NN (usually the next instruction is a jump to skip a code block).
+      0x4: () => this.skipNextInstructionIf(this.V[X] !== NN), //Skips the next instruction if VX does not equal NN (usually the next instruction is a jump to skip a code block).
+      0x5: () => this.skipNextInstructionIf(this.V[X] === this.V[Y]), //Skips the next instruction if VX equals VY (usually the next instruction is a jump to skip a code block).
+      0x6: () => this.V[X] = NN, //Sets VX to NN.
+      0x7: () => this.V[X] = this.V[X] + NN, //Adds NN to VX (carry flag is not changed).
       0x8: (opcode, X, Y) => this.family8(opcode, X, Y),
-      0x9: undefined, //Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block).
-      0xA: undefined, //Sets I to the address NNN.
-      0xB: () => { this.pc = this.V[0] + NNN }, //Jumps to the address NNN plus V0.
-      0xC: undefined, //Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
-      0xD: undefined, 
+      0x9: () => this.skipNextInstructionIf(this.V[X] !== this.V[Y]), //Skips the next instruction if VX does not equal VY. (Usually the next instruction is a jump to skip a code block).
+      0xA: () => this.I = NNN, //Sets I to the address NNN.
+      0xB: () => this.pc = this.V[0] + NNN, //Jumps to the address NNN plus V0.
+      0xC: () => this.V[X] = this.getRandomInt(255) & NN, //Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
+      0xD: () => this.drawSpriteAt(this.V[X], this.V[Y], N), //Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
       0xE: (opcode) => this.familyE(opcode), 
       0xF: (opcode) => this.familyF(opcode), 
     }
@@ -127,8 +125,63 @@ class Chip8Display {
     return undefined
   }
 
+  skipNextInstructionIf(condition) {
+    if (condition) {
+      this.pc += 2
+    }
+  }
+
   family8(opcode, X, Y) {
     return undefined
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  drawPixelAt(x,y){
+    // wrap
+    x = x % 64;
+    y = y % 32;
+
+    const index = x + (y * 64)
+    if(this.debugDisplay[index] === 0) {
+      this.canvasContext.fillStyle = 'white';
+      this.canvasContext.fillRect(x * this.SCALE, y * this.SCALE, this.SCALE, this.SCALE);
+      this.debugDisplay[index] = 1
+    } else {
+      this.canvasContext.fillStyle = 'black';
+      this.canvasContext.fillRect(x * this.SCALE, y * this.SCALE, this.SCALE, this.SCALE);
+      this.V[0xF] = 1 //It resets in drawSpriteAt() 
+      this.debugDisplay[index] = 0
+    }
+  }
+
+  drawByteAt(x, y, byte){
+    if (byte < 0) throw new Error(`drawByte - byte ${byte} is below 0x00`);
+    if (byte > 255) throw new Error(`drawByte - byte ${byte} exceeds 0xFF value`);
+    let variableX = x
+
+    for (let position = 7; position >= 0; position--) {
+      const bit = (byte >> position) & 1;
+      if (bit) {
+        this.drawPixelAt(variableX, y)
+      }
+      variableX++
+    }
+  }
+
+  drawSpriteAt(x,y,height){
+    this.V[0xF] = 0
+    let memoryIndex = this.I
+    let variableY = y
+
+    for (let idx1 = 0; idx1 < height; idx1++) {
+      let byte = this.memory[memoryIndex]
+      this.drawByteAt(x, variableY, byte)
+      memoryIndex++
+      variableY++
+    }
   }
 
   startTimers(){
@@ -149,7 +202,7 @@ class Chip8Display {
     }
     const opcode = (this.memory[this.pc] << 8) | this.memory[this.pc + 1];
     this.opcode = opcode;
-    this.pc += 2;
+    this.pc += 2; //points to the next instruction 
     this.decodeAndExecuteOpcode(opcode);
   }
 
@@ -161,7 +214,8 @@ class Chip8Display {
 
 }
 
-const chip8Display = new Chip8Display(displayElement);
+const chip8 = new Chip8(displayElement);
 
+chip8.drawByteAt(0,1,245)
 
-chip8Display.startEmulation()
+chip8.startEmulation()
